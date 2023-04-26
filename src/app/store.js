737f0ab7e86bc4736d291e4acff77568f2b9b570
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-04-26 23:03:42 +0530

    removed counter

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+
 import canvasReducer from '../features/canvas/canvaSlicer';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-    canvas: canvasReducer,
+    canvas: canvasReducer
   },
 });
